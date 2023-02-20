6627c5e9455c1d411df4b5ce09dcdb8f6692d9a7
TechLearner <sanketsatarkar@gmail.com>
2023-02-20 11:55:36 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="swagger" id="swagger"></div>
+</template>
+
+<script>
+import SwaggerUI from "swagger-ui";
+import "swagger-ui/dist/swagger-ui.css";
+
+export default {
+  name: "Swagger",
+  mounted() {
+    SwaggerUI({
+      url: "https://petstore3.swagger.io/api/v3/openapi.json",
+      dom_id: "#swagger",
+    });
+  },
+};
+</script>
